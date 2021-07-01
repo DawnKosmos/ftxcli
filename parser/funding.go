0f5c15f2f2774285a76fc281e66f6f67adfa5afa
DawnKK <martin_hottmann@yahoo.com>
2021-07-01 16:44:51 +0200

    funding parser added

--- parser/funding.go
+++ parser/funding.go
@@ -1,5 +1,10 @@
 package parser
 
+import (
+	"errors"
+	"strconv"
+)
+
 /*
 
 funding -position //funding rate der aktuellen positionen
@@ -20,11 +25,50 @@ const (
 
 type Funding struct {
 	ft        FundingType
-	Ticker    string
+	Ticker    []string
 	Time      int64
 	Summarize bool
 }
 
 func ParseFunding(tl []Token) (*Funding, error) {
+	var fund Funding
+	fund.ft = PAYMENTS
 
+	for _, v := range tl {
+		switch v.Type {
+		case FLAG:
+			switch v.Text {
+			case "sum":
+				fund.Summarize = true
+			case "position":
+				fund.ft = POSITIONS
+			}
+		case VARIABLE:
+			fund.Ticker = append(fund.Ticker, v.Text)
+		case DURATION:
+			ss := v.Text
+			n, err := strconv.Atoi(ss[:len(ss)-1])
+			if err != nil {
+				return nil, err
+			}
+			switch ss[len(ss)-1] {
+			case 'h':
+				n *= 3600
+			case 'm':
+				n *= 60
+			case 'd':
+				n *= 3600 * 24
+			default:
+				return &fund, errors.New(v.Text + "I dont know how you fucked that up")
+			}
+			fund.Time = int64(n)
+		case FLOAT:
+			ff, err := strconv.ParseFloat(v.Text, 64)
+			if err != nil {
+				return nil, err
+			}
+			fund.Time = int64(ff) * 3600
+		}
+	}
+	return &fund, nil
 }
