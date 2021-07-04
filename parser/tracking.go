fb2a4d83230685108fe7ff10d4aa4af79a8ccfae
DawnKK <martin_hottmann@yahoo.com>
2021-07-04 13:08:28 +0200

    tracking description

--- parser/tracking.go
+++ parser/tracking.go
@@ -5,4 +5,14 @@ Tracking soll alles was auf dem Account passiert tracken und in einer Datei spei
 Diese Datei kann später benutzt werden um Statistiken zu erstellen und .csv dateien zu generieren
 bzw. Statistiken auswerten
 
+
+
+Daten werden in JSON format getrackt
+jeder tag ist ein eintrag. jeder monat ist ein file
+
+Im .acc file muss eine genehmigung fürs tracken drin sein
+dann wird ein file in einem order gestellt und gefragt für wie viel zeit zurückgeschaut werden soll
+
+Ein webseite soll erstellt werden mit allen nützlichen informationen wenn gefordert wird
+
 */
